c58d001309404a8a3d86e62517b1d057be606055
Thomas Brunko <tsbo@gft.com>
2022-05-30 16:32:42 +0200

    First working version.

--- requestlogger/configuration.go
+++ requestlogger/configuration.go
@@ -3,21 +3,68 @@ package requestlogger
 import (
 	"flag"
 	"os"
+  "strconv"
 
 	"github.com/com-gft-tsbo-source/go-common/ms-framework/microservice"
 )
 
+// LoggingConfiguration ...
+type LoggingConfiguration struct {
+  LineLength int
+  LogHeaders bool
+  LogPayload bool
+  EncodePayload bool
+}
+
+// ILoggingConfiguration ...
+type ILoggingConfiguration interface {
+	GetLineLength() int
+	SetLineLength(int)
+	GetLogHeaders() bool
+	SetLogHeaders(bool)
+	GetLogPayload() bool
+	SetLogPayload(bool)
+	GetEncodePayload() bool
+	SetEncodePayload(bool)
+}
+
 // Configuration ...
 type Configuration struct {
 	microservice.Configuration
+  LoggingConfiguration
 }
 
 // IConfiguration ...
 type IConfiguration interface {
 	microservice.IConfiguration
+  ILoggingConfiguration
 }
 
 // ---------------------------------------------------------------------------
+
+// GetLineLength ...
+func (cfg *LoggingConfiguration) GetLineLength() int { return cfg.LineLength }
+
+// SetLineLength ...
+func (cfg *LoggingConfiguration) SetLineLength(v int) { cfg.LineLength = v }
+
+// GetLogHeaders ...
+func (cfg *LoggingConfiguration) GetLogHeaders() bool { return cfg.LogHeaders }
+
+// SetLogHeaders ...
+func (cfg *LoggingConfiguration) SetLogHeaders(v bool) { cfg.LogHeaders = v }
+
+// GetLogPayload ...
+func (cfg *LoggingConfiguration) GetLogPayload() bool { return cfg.LogPayload }
+
+// SetLogPayload ...
+func (cfg *LoggingConfiguration) SetLogPayload(v bool) { cfg.LogPayload = v }
+
+// GetEncodePayload ...
+func (cfg *LoggingConfiguration) GetEncodePayload() bool { return cfg.EncodePayload }
+
+// SetEncodePayload ...
+func (cfg *LoggingConfiguration) SetEncodePayload(v bool) { cfg.EncodePayload = v }
 
 // InitConfigurationFromArgs ...
 func InitConfigurationFromArgs(cfg *Configuration, args []string, flagset *flag.FlagSet) {
@@ -25,7 +72,55 @@ func InitConfigurationFromArgs(cfg *Configuration, args []string, flagset *flag.
 		flagset = flag.NewFlagSet("requestlogger", flag.PanicOnError)
 	}
 
+	plineLength := flagset.Int("lineLength", -1, "Maximal line length of log output.")
+	plogHeaders := flagset.Bool("logHeaders", false, "Also log headers.")
+	plogPayload := flagset.Bool("logPayload", false, "Also log payload.")
+
 	microservice.InitConfigurationFromArgs(&cfg.Configuration, args, flagset)
 	flagset.Parse(os.Args[1:])
 
+	if *plineLength >= 0 {
+		cfg.SetLineLength(*plineLength)
+	} else {
+		ev := os.Getenv("REQUESTLOGGER_LINELENGTH")
+		if len(ev) > 0 {
+			v, err := strconv.Atoi(ev)
+			if err != nil {
+				panic(err)
+			}
+			cfg.SetLineLength(v)
+		} else {
+			cfg.SetLineLength(-1)
+		}
+	}
+
+	if *plogHeaders {
+		cfg.SetLogHeaders(*plogHeaders)
+	} else {
+		ev := os.Getenv("REQUESTLOGGER_LOGHEADERS")
+		if len(ev) > 0 {
+			v, err := strconv.Atoi(ev)
+			if err != nil {
+				panic(err)
+			}
+			cfg.SetLogHeaders(v != 0)
+		} else {
+			cfg.SetLogHeaders(false)
+		}
+	}
+
+	if *plogPayload {
+		cfg.SetLogPayload(*plogPayload)
+	} else {
+		ev := os.Getenv("REQUESTLOGGER_LOGPAYLOAD")
+		if len(ev) > 0 {
+			v, err := strconv.Atoi(ev)
+			if err != nil {
+				panic(err)
+			}
+			cfg.SetLogPayload(v != 0)
+		} else {
+			cfg.SetLogPayload(false)
+		}
+	}
 }
--- requestlogger/main.go
+++ requestlogger/main.go
@@ -1,8 +1,11 @@
 package requestlogger
 
 import (
+	"io"
 	"flag"
 	"net/http"
+  "strings"
+  "unicode"
 
 	"github.com/com-gft-tsbo-source/go-common/ms-framework/microservice"
 )
@@ -16,19 +19,79 @@ import (
 // RequestLogger Encapsulates the requestlogger data
 type RequestLogger struct {
 	microservice.MicroService
+  *LoggingConfiguration
 }
 
 // ###########################################################################
 
 // httpLogRequest ...
 func (ms *RequestLogger) httpLogRequest(w http.ResponseWriter, r *http.Request) (status int, contentLen int, msg string) {
-  //	msg = fmt.Sprintf("'%s' @ '%s' called.", ms.GetName(), ms.GetVersion())
 	var response microservice.Response
+  var maxLen int
 
 	microservice.InitResponseFromMicroService(&response, ms, msg)
 	ms.SetResponseHeaders("application/json; charset=utf-8", w, r)
 	w.WriteHeader(http.StatusOK)
 	contentLen = ms.Reply(w, response)
+  ms.GetLogger().Printf("| %-6.6s | %s\n", r.Method, r.URL.String())
+
+  if ms.GetLogHeaders() {
+
+    for name, _ := range r.Header {
+      if len(name) > maxLen {
+        maxLen = len(name)
+      }
+    }
+
+    for name, values := range r.Header {
+      for _, value := range values {
+        ms.GetLogger().Printf("  %6.6s | %-*s = '%s'\n", "H", maxLen, name, value)
+      }
+    }
+
+  }
+
+  defer r.Body.Close()
+
+  if ms.GetLogPayload() {
+
+    var hasLines bool
+    var pairs []string
+
+    body := new(strings.Builder)
+    n, _ := io.Copy(body, r.Body)
+    str := body.String()
+
+    if n > 0 {
+
+      str = strings.Map(func(r rune) rune {
+        if unicode.IsPrint(r) {
+          return r
+        }
+         return rune('?')
+      }, str)
+
+
+      hasLines = true
+
+      for hasLines = true; hasLines; hasLines = len(pairs) > 1 {
+
+        pairs = strings.SplitN(str, "\n", 2)
+        ms.GetLogger().Printf("  %6.6s | %s\n", "P", pairs[0])
+
+        if len(pairs) > 1 {
+          str = pairs[1]
+        } else {
+          str = ""
+        }
+      }
+      if len(str) > 0 {
+        ms.GetLogger().Printf("  %-6.6s | %s\n", "", str)
+      }
+    }
+
+  }
+
 	return status, contentLen, msg
 }
 // ###########################################################################
@@ -41,10 +104,18 @@ func InitFromArgs(ms *RequestLogger, args []string, flagset *flag.FlagSet) *Requ
 		flagset = flag.NewFlagSet("requestlogger", flag.PanicOnError)
 	}
 
+	handler := ms.DefaultHandler()
+  handler.Any = ms.httpLogRequest
+  handler.Get = ms.httpLogRequest
+  handler.Put = ms.httpLogRequest
+  handler.Post = ms.httpLogRequest
+  handler.Delete = ms.httpLogRequest
+  handler.Head = ms.httpLogRequest
+  handler.Connect = ms.httpLogRequest
+  handler.Options = ms.httpLogRequest
 	InitConfigurationFromArgs(&cfg, args, flagset)
-	microservice.Init(&ms.MicroService, &cfg.Configuration, nil)
-
-//	ms.AddHandler("/template", templateHandler)
+	ms.LoggingConfiguration = &cfg.LoggingConfiguration
+	microservice.Init(&ms.MicroService, &cfg.Configuration, handler)
 
 	return ms
 }
